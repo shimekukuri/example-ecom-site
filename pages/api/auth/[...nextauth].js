890e7149e413f66292cca105e52b7ac54be9efea
Tyler Hutchinson <jhutchinson192390@gmail.com>
2022-10-21 11:19:12 -0400

    added authentication api

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,1 +1,44 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
+import User from "../../../models/User";
+import db from "../../../utils/db";
+import bcrypt from "bcryptjs";
+import CredentialProvider from "next-auth/providers/credentials";
+
+export default NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user?._id) token._id = user._id;
+      if (user?.isAdmin) token._isAdmin = user.isAdmin;
+      return token;
+    },
+    async seesion({ session, token }) {
+      if (token?._id) session.user._id = token._id;
+      if (token?.isAdmin) session.user.isAdmin = token.isAdmin;
+      return session;
+    },
+  },
+  providers: [
+    CredentialProvider({
+      async authorize(credentials) {
+        await db.connect();
+        const user = await User.findOne({
+          email: credentials.email,
+        });
+        db.disconnect();
+        if (user && bcrypt.compareSync(credentials.password, user.password)) {
+          return {
+            user: user._id,
+            name: user.name,
+            email: user.email,
+            image: "f",
+            isAdmin: user.isAdmin,
+          };
+        }
+        throw new Error("Problem authenticating UserName or Password");
+      },
+    }),
+  ],
+});
