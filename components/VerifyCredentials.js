f32ffd0cd747f50b14812cf36f3ccbe9869acb63
Tyler Hutchinson <jhutchinson192390@gmail.com>
2022-10-27 13:53:42 -0400

    Overlay to handle credential verification

---
+++ components/VerifyCredentials.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import verify from "../utils/verify";
+import { XCircleIcon } from "@heroicons/react/outline";
+
+export default function VerifyCredentials(props) {
+  const { setVerifyHandler, setForcedExit } = props;
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const submitHandler = ({ email, password }) => {
+    verify(email, password).then((data) => setVerifyHandler(data));
+  };
+
+  return (
+    <div className="opacity-animation-1000 opacity-0 z-20 bg-white bg-opacity-25 w-screen h-full absolute top-0 left-0 flex justify-center items-center">
+      <form
+        className="max-w-screen-md m-auto px-4"
+        onSubmit={handleSubmit(submitHandler)}
+      >
+        <div className="flex justify-end items-center">
+          <XCircleIcon className="w-8" onClick={() => setForcedExit(false)} />
+        </div>
+        <label htmlFor="email">Email Address:</label>
+        <input
+          type="email"
+          id="email"
+          className="w-full mb-2 shadow-xl"
+          autoFocus
+          {...register("email", {
+            required: "Please Enter an address",
+            pattern: {
+              value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+              message: "Please enter a valid Email Address",
+            },
+          })}
+        ></input>
+        {errors.email && (
+          <div className="text-red-600 text-lg">{errors.email.message}</div>
+        )}
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          className="w-full mb-2"
+          autoFocus
+          {...register("password", {
+            required: "Please Enter a Password",
+            minLength: {
+              value: 8,
+              message: "Please enter a password with atleast 8 charecters",
+            },
+          })}
+        ></input>
+        {errors.password && (
+          <div className="text-red-600 text-lg">{errors.password.message}</div>
+        )}
+        <div className="w-full flex justify-center mb-8">
+          <button className="primary-button">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}
