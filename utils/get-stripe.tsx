b3a74fa82cdf8176bd6f68a68ca655ef5241db14
Tyler Hutchinson <jhutchinson192390@gmail.com>
2022-11-02 14:34:52 -0400

    fixed get stripe and added in interfaces

--- utils/get-stripe.tsx
+++ utils/get-stripe.tsx
@@ -1,11 +1,12 @@
-import { loadStripe, Stripe } from "@stripe/stripe-js";
+import { loadStripe } from "@stripe/stripe-js/pure";
+import { Stripe } from "@stripe/stripe-js";
 
 let stripePromise: Promise<Stripe | null>;
 const getStripe = () => {
-  if(!stripePromise) {
+  if (!stripePromise) {
     stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
   }
   return stripePromise;
-}
+};
 
 export default getStripe;
