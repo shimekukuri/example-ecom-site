75af077fa9a11b8e619007b09aabbeeaebe844f4
Tyler Hutchinson <jhutchinson192390@gmail.com>
2022-11-03 11:06:25 -0400

    Begining of final checkout/confirm page

--- pages/finalCheckout.js
+++ pages/finalCheckout.js
@@ -8,22 +8,71 @@ import { ACTIONS } from "../utils/ACTIONS";
 
 export default function FinalCheckout(props) {
   const [loading, setLoading] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(true);
   const { status, data: session } = useSession();
+
   const { state, dispatch } = useContext(UserState);
+  const {
+    cart: { cartItems },
+    paymentMethod,
+  } = state;
 
   const router = useRouter();
+  let paymentStatus
   const { session_id } = router.query;
+  ({status: paymentStatus} = router.query);
+
+  const round2 = (num) => Math.round(num * 100 + Number.EPSILON) / 100;
+
+  const itemsPrice = round2(
+    cartItems.reduce((t, c) => t + c.quantity * c.price, 0)
+  );
+
+  const shippingPrice = itemsPrice > 200 ? 0 : 15;
+  const taxPrice = round2(itemsPrice * 0.15);
+  const totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
 
   useEffect(() => {
-    console.log(session_id, state, session);
+    const timerid = setTimeout(() => {
+      console.log(session_id, state, status, paymentStatus);
+    }, 1000);
     setLoading(true);
+    // fetch("/api/updateorder", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     userEmail: session.user.email,
+    //     shippingAddress: state.cart.shippingAddress,
+    //     paymentMethod: paymentMethod,
+    //     itemsPrice: itemsPrice,
+    //     shippingPrice: shippingPrice,
+    //     taxPrice: taxPrice,
+    //     totalPrice: totalPrice,
+    //     isPaid: status === "success" ? true : false,
+    //     isDelivered: false,
+    //     paidAt: Date.now(),
+    //     deliveredAt: "",
+    //   }),
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     setLoading(false);
+    //     if (data.success === true) {
+    //       setShowSuccess(true);
+    //     }
+    //   });
 
-
+    return () => {
+      clearTimeout(timerid);
+    };
   }, [session_id]);
 
   return (
     <Layout title="Final Checkout">
       <CheckOut step={4} />
+      {showSuccess && <div>Worked</div>}
     </Layout>
   );
 }
